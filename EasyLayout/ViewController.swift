28dc7ddc00340bafc4b15d84b01bf172338d3cdc
Andrey Krasivichev <21ghost@mail.ru>
2020-04-20 17:16:55 +0300

    Initial Commit

---
+++ EasyLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyLayout
+//
+//  Created by Andrey Krasivichev on 20.04.2020.
+//  Copyright © 2020 Andrey Krasivichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
