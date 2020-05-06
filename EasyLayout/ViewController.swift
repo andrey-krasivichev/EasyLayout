9ccf6a5991340139aa0e2559e3c44b78d72bae90
Andrey Krasivichev <21ghost@mail.ru>
2020-05-06 10:47:57 +0300

    Convenience enhancements

--- EasyLayout/ViewController.swift
+++ EasyLayout/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(scrollView)
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.green
+        self.scrollView.backgroundColor = UIColor.cyan
+        self.setupConstraints()
     }
 
+    // MARK: Private
+    
+    private func setupConstraints() {
+        let scrollView = self.scrollView
+        
+        scrollView.makeConstraints { (make) in
+            make.left.right.equalTo(self.view)
+            make.top.equalTo(self.view).offset(30.0)
+            make.height.equalTo(self.view).multipliedBy(0.3)
+        }
+        
+        let firstSubview: UIView = UIView()
+        firstSubview.backgroundColor = UIColor.blue
+        scrollView.addSubview(firstSubview)
+        firstSubview.makeConstraints { (make) in
+            make.centerX.top.equalTo(scrollView)
+            make.width.equalTo(200.0)
+            make.height.equalTo(80.0)
+        }
+        
+        let secondSubview: UIView = UIView()
+        secondSubview.backgroundColor = UIColor.red
+        scrollView.addSubview(secondSubview)
+        secondSubview.makeConstraints { (make) in
+            make.top.equalTo(firstSubview.cm.bottom).offset(20.0)
+            make.width.equalTo(firstSubview).multipliedBy(0.5)
+            make.centerX.equalTo(scrollView)
+            make.height.equalTo(120.0)
+        }
+        
+        let thirdSubview: UIView = UIView()
+        thirdSubview.backgroundColor = UIColor.magenta
+        scrollView.addSubview(thirdSubview)
+        thirdSubview.makeConstraints { (make) in
+            make.centerX.equalTo(secondSubview)
+            make.top.equalTo(secondSubview.cm.bottom).offset(20.0)
+            make.width.equalTo(100.0)
+            make.height.equalTo(400.0)
+            make.bottom.equalTo(scrollView).priority(100.0)
+            make.bottom.equalTo(scrollView).offset(-20.0)
+        }
+    }
 
-}
+}   
 
